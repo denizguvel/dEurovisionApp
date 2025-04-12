9fab30c72086054f7e10f64960d29a1765a78e51
Deniz Güvel <deniz.guvel@gmail.com>
2025-04-13 01:43:03 +0300

    added year parameter

--- eurovision_app/lib/app/features/data/datasources/remote/eurovision_remote_datasource.dart
+++ eurovision_app/lib/app/features/data/datasources/remote/eurovision_remote_datasource.dart
@@ -1,66 +1,3 @@
-// import 'package:eurovision_app/app/features/data/models/contest_model.dart';
-// import 'package:eurovision_app/app/features/data/models/contestant_model.dart';
-// import 'package:eurovision_app/core/config/env_config.dart';
-// import 'package:eurovision_app/core/dio_manager/api_response_model.dart';
-// import 'package:eurovision_app/core/dio_manager/dio_manager.dart';
-
-// abstract class EurovisionRemoteDatasource {
-//   Future<ApiResponseModel<List<ContestModel>>> fetchContestYear({int year});
-//    Future<List<ContestModel>> fetchAllContests();
-// }
-
-// final class EurovisionRemoteDatasourceImpl implements EurovisionRemoteDatasource {
-//   final DioApiManager _dioApiManager =
-//       DioApiManager(baseUrl: EnvConfig.eurovisionBaseUrl);
-
-//   @override
-//   Future<ApiResponseModel<List<ContestModel>>> fetchContestYear({int year = 2024}) async {
-//     var apiResponseModel = await _dioApiManager.get<List<ContestModel>>(
-//       '/contests',
-//       queryParams: {'year': year},
-//       converter: (data) {
-//         if (data is List) {
-//           return data.map((e) => ContestModel.fromJson(e)).toList();
-//         } else {
-//           return <ContestModel>[];
-//         }
-//       },
-//     );
-
-//     return apiResponseModel;
-//   }
-
-//   Future<List<ContestModel>> fetchAllContests() async {
-//     final response = await _dioApiManager.get<List<ContestModel>>(
-//       '/contests',
-//       converter: (data) {
-//         if (data is List) {
-//           return data.map((e) => ContestModel.fromJson(e)).toList();
-//         } else {
-//           return <ContestModel>[];
-//         }
-//       },
-//     );
-//     return response.data ?? [];
-//   }
-
-
-//   Future<ContestantModel?> fetchWinnerByYear(int year) async {
-//     final response = await _dioApiManager.get<Map<String, dynamic>>(
-//       '/contests/$year',
-//       converter: (data) => data,
-//     );
-
-//     final contestants = response.data?['contestants'] as List?;
-//     if (contestants != null && contestants.isNotEmpty) {
-//       return ContestantModel.fromJson(contestants[0]);
-//     }
-
-//     return null;
-//   }
-
-// }
-
 import 'package:eurovision_app/app/features/data/models/contest_model.dart';
 import 'package:eurovision_app/app/features/data/models/contestant_model.dart';
 import 'package:eurovision_app/core/config/env_config.dart';
@@ -117,10 +54,9 @@ final class EurovisionRemoteDatasourceImpl implements EurovisionRemoteDatasource
 
     final contestants = response.data?['contestants'] as List?;
     if (contestants != null && contestants.isNotEmpty) {
-      return ContestantModel.fromJson(contestants[0]);
+      return ContestantModel.fromJson(contestants[0], year);
     }
 
     return null;
   }
 }
-
