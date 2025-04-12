81c08a506ebb7c1d12da28c83eb66748c97e783e
Deniz Güvel <deniz.guvel@gmail.com>
2025-04-13 01:36:09 +0300

    App Icon changes

--- eurovision_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ eurovision_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,12 @@ import Foundation
 
 import connectivity_plus
 import path_provider_foundation
+import video_player_avfoundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
