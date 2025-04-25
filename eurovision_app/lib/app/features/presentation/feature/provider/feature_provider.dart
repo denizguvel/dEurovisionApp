47f234e2947982b738a5c271ae85f44891455225
Deniz Güvel <deniz.guvel@gmail.com>
2025-04-25 14:23:06 +0300

    Changed gradient colors

--- eurovision_app/lib/app/features/presentation/feature/provider/feature_provider.dart
+++ eurovision_app/lib/app/features/presentation/feature/provider/feature_provider.dart
@@ -217,7 +217,7 @@ Future<void> getCountryWins() async {
   // ----------------------------- Gradient -----------------------------
   final GetGradientUseCase _getGradientUseCase = GetGradientUseCase();
   GradientEntity _gradient = GradientEntity(
-    colors: [AppColors.black, AppColors.pinkyPink, AppColors.black],
+    colors: [AppColors.black, AppColors.black, AppColors.pinkyPink, AppColors.black, AppColors.black],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
